3a563290a434e10e65175ae0e0ae244874892fd9
Stanislav <spirit9969@gmail.com>
2021-12-08 22:16:24 +0200

    Updates

---
+++ precache-manifest.96b75eb3919f4c43afb9b4da2a74f6be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554acaed396de98c3c4a18a52afa6a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a324303844a2072ca2",
+    "url": "/static/css/main.8eed7bab.chunk.css"
+  },
+  {
+    "revision": "aaa2084419775f70ec23",
+    "url": "/static/js/2.bbb1be49.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.bbb1be49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a324303844a2072ca2",
+    "url": "/static/js/main.376a1135.chunk.js"
+  },
+  {
+    "revision": "ff3c7540f2bdd0a92916",
+    "url": "/static/js/runtime-main.f0bb1af4.js"
+  },
+  {
+    "revision": "6617f447369d9c9491b67bb82766110e",
+    "url": "/static/media/error.6617f447.gif"
+  },
+  {
+    "revision": "dd8446a3584cb7275fb1f2f31ab4c950",
+    "url": "/static/media/mjolnir.dd8446a3.png"
+  },
+  {
+    "revision": "76e8b3e15712a0d9c704b99e10476e4c",
+    "url": "/static/media/vision.76e8b3e1.png"
+  }
+]);
