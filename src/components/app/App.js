5a055d59fe8a74b731e88bb5d38010fc682248b3
Stanislav <spirit9969@gmail.com>
2021-12-12 22:13:27 +0200

    use Routing react-rout0dom v.6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { useState } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 
+import { MainPage, ComicsPage } from "../pages";
+
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../erroeBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/comicsList";
-import AppBanner from "../appBanner/AppBanner";
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
 
     return (
         <Router>
@@ -26,34 +14,9 @@ const App = () => {
                 <AppHeader />
                 <main>
                     <Routes>
-                        <Route exect path="/" element={
-                            <>
-                                <ErrorBoundary>
-                                    <RandomChar />
-                                </ErrorBoundary>
-                                <div className="char__content">
-                                    <ErrorBoundary>
-                                        <CharList onCharSelected={onCharSelected} />
-                                    </ErrorBoundary>
-                                    <ErrorBoundary>
-                                        <CharInfo charId={selectedChar} />
-                                    </ErrorBoundary>
-                                </div>
-                                <img className="bg-decoration" src={decoration} alt="vision" />
-                            </>
-                        }>
-
-                        </Route>
-                        <Route exect path="/comics" element={
-                            <>
-                                <AppBanner />
-                                <ComicsList />
-                            </>
-                        }>
-                        </Route>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/comics" element={<ComicsPage />} />
                     </Routes>
-
-
                 </main>
             </div>
         </Router>
